9cfbe6d70f38bc09c3349b979152056cf360b798
Gonzalo <52017826+GonzaloRocha-98@users.noreply.github.com>
2021-01-13 18:03:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-document.getElementById("buscar").addEventListener('submit', function(){
-    const texto = document.getElementById("texto").value.trim().toLowerCase().split(' ');
+document.getElementById("buscar").addEventListener('submit', function(e){
+    //const texto = document.getElementById("texto").value.toLowerCase().split(' ');
+    const texto = document.getElementById("texto").value.toLowerCase();
     const palabraABuscar = document.getElementById("palabra").value;
     let repeticiones = 0;
-    texto.forEach(palabra => {
+    /*texto.forEach(palabra => {
         if (palabra.match(palabraABuscar)){
             repeticiones++;
         }
-    });
+    });*/
+    let i= 0;
+    while (i !== -1){
+    	i= texto.indexOf(palabraABuscar,i);
+    	if(i !== -1){
+    		i++;
+		repeticiones++;
+	}	    
+    }
     let element = document.createElement('div');
     element.innerHTML = `
         <strong>Se encontraron ${repeticiones} coincidencias </strong>
@@ -16,4 +25,5 @@ document.getElementById("buscar").addEventListener('submit', function(){
 		coincidencias.removeChild(coincidencias.firstChild)
 	}
     coincidencias.appendChild(element)
+    e.preventDefault();	
 });
